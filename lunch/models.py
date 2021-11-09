22cf055dd1a132082da7dfa0cf2e0a25b101bcd3
Masum_Osman <masum@trucklagbe.com>
2021-11-09 16:13:09 +0600

    models created with the initial starting

---
+++ lunch/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.db.models.fields import DateField
+
+# Create your models here.
+
+class Restaurants(models.Model):
+    restaurantId = models.AutoField(primary_key=True)
+    restaurantName = models.CharField(max_length=220)
+    ownerName = models.CharField(max_length=220)
+    createdOn = models.DateField()
+
+
+class Employees(models.Model):
+    employeeId = models.AutoField(primary_key=True)
+    employeeName = models.CharField(max_length=220)
+    department = models.CharField(max_length=220)
+    dateOfJoining = models.DateField()
+
+
+class Menu(models.Model):
+    menuId = models.AutoField(primary_key=True)
+    restaurantId = models.IntegerField(11)
+    title = models.CharField(200)
+    type = models.CharField(200)
+    summery = models.CharField(200)
+    price = models.IntegerField(11)
+    createdOn = models.DateField()
+    
+
+class Vote(models.Model):
+    voteId = models.AutoField(primary_key=True)
+    employeeId = models.IntegerField(11)
+    menuId = models.IntegerField(11)
+    votedOn = models.DateField()
