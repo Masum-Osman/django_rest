6655f195077d643c8bccfe385b5bd5c8ca0f5502
Masum_Osman <masum@trucklagbe.com>
2021-11-10 21:22:03 +0600

    restaurant post added

--- lunch/views.py
+++ lunch/views.py
@@ -14,3 +14,10 @@ def restaurantApi(request, id=0):
         restaurant = Restaurants.objects.all()
         restaurant_serializer = RestaurantSerializer(restaurant, many=True)
         return JsonResponse(restaurant_serializer.data, safe=False)
+    elif request.method == 'POST':
+        restaurant_data = JSONParser().parse(request)
+        restaurant_serializer = RestaurantSerializer(data=restaurant_data)
+        if restaurant_serializer.is_valid():
+            restaurant_serializer.save()
+            return JsonResponse("restaurant added successfully", safe=False)
+        return JsonResponse("failed to add new restaurant", safe=False)
