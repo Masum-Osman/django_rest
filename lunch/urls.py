d5cc3148fb67d9eeb7b12fc0c188abef57859129
Masum-Osman <ogkmasum@gmail.com>
2021-11-12 18:56:22 +0600

    url in sub api

--- lunch/urls.py
+++ lunch/urls.py
@@ -6,17 +6,12 @@ urlpatterns=[
     url(r'^restaurant$', views.restaurantApi),
     url(r'^restaurant/([0-9]+)$', views.restaurantApi),
 
-    url(r'^menu$',views.menuApi),
-    url(r'^menu/([0-9]+)$',views.menuApi),
+    url(r'^restaurant/menu$',views.menuApi),
+    url(r'^restaurant/menu/([0-9]+)$',views.menuApi),
+
+    url(r'^restaurant/menu/vote$',views.voteApi),
+    url(r'^restaurant/menu/vote/([0-9]+)$',views.voteApi),
 
     url(r'^employee$',views.employeeApi),
     url(r'^employee/([0-9]+)$',views.employeeApi),
-
-    url(r'^vote$',views.voteApi),
-    url(r'^vote/([0-9]+)$',views.voteApi),
 ]
-
-# URLPattern=[
-#     url(r'^restaurant$', views.restaurantApi),
-#     url(r'^restaurant/([0-9]+)$', views.restaurantApi)
-# ]
